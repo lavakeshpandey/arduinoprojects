5de5b3bff97319eb583379dfc70a271a7f535880
Lavakesh Pandey <jsonbourneagain@outlook.com>
2020-07-17 23:33:15 +0530

    pwm version 2

--- pulseWidthModulation/pulseWidthModulation.ino
+++ pulseWidthModulation/pulseWidthModulation.ino
@@ -12,51 +12,20 @@ void loop() {
   // put your main code here, to run repeatedly:
   for(int a=0; a<256; a++){
     analogWrite(3, a);
-    delay(d);
-  }
-  for(int a =255; a>=0; a--){
-    analogWrite(3,a);
-    delay(d);
-  }
-  delay(200);
-
-  for(int a=0; a<256; a++){
     analogWrite(5, a);
-    delay(d);
-  }
-  for(int a =255; a>=0; a--){
-    analogWrite(5,a);
-    delay(d);
-  }
-  delay(200);
-
-  for(int a=0; a<256; a++){
     analogWrite(6, a);
-    delay(d);
-  }
-  for(int a =255; a>=0; a--){
-    analogWrite(6,a);
-    delay(d);
-  }
-  delay(200);
-
-  for(int a=0; a<256; a++){
     analogWrite(9, a);
-    delay(d);
-  }
-  for(int a =255; a>=0; a--){
-    analogWrite(9,a);
-    delay(d);
-  }
-  delay(200);
-
-  for(int a=0; a<256; a++){
     analogWrite(11, a);
     delay(d);
   }
   for(int a =255; a>=0; a--){
-    analogWrite(11,a);
+    analogWrite(3,a);
+    analogWrite(5, a);
+    analogWrite(6, a);
+    analogWrite(9, a);
+    analogWrite(11, a);
     delay(d);
   }
   delay(200);
+
 }
